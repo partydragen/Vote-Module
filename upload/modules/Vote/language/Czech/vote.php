911199a52145a274d10e6c4dc415d82e591e6189
Fjuro <40403606+Fjuro@users.noreply.github.com>
2021-03-08 10:11:58 +0100

    Translate module
    Translated the module to the Czech language

---
+++ upload/modules/Vote/language/Czech/vote.php
@@ -0,0 +1,48 @@
+<?php 
+/*
+ *	Made by Partydragen and Samerton
+ *  Translated by Fjuro
+ *  https://github.com/partydragen/Vote-Module
+ *  https://partydragen.com
+ *  NamelessMC version 2.0.0-pr8
+ *
+ *  License: MIT
+ *
+ *  Czech Language for Vote module
+ */
+
+$language = array( 
+	'vote' => 'Hlasování',
+	'vote_sites' => 'Hlasovací stránky',
+	'vote_name' => 'Název hlasovací stránky',
+	'no_vote_sites' => 'Nebyly nastaveny žádné hlasovací stránky',
+	'vote_url' => 'URL hlasovací stránky <em>(s předchozím http://)</em>',
+	'new_site' => 'Přidat hlasovací stránku',
+	'edit_site' => 'Úprava stránky',
+	'delete_site' => 'Opravdu chcete odstranit tuto stránku?',
+	'message' => 'Zpráva k zobrazení na vrchu hlasovací stránky <em>(může být ponecháno prázdné)</em>',
+	'message_maximum' => 'Hlasovací zpráva může obsahovat maximálně 2048 znaků',
+	'link_location' => 'Umístění odkazu',
+	'icon' => 'Ikona',
+	'icon_example' => 'Příklad: <i class="fa fa-thumbs-up" aria-hidden="true"></i>',
+	
+	// Success
+	'site_created_successfully' => 'Stránka úspěšně přidána.',
+	'site_updated_successfully' => 'Stránka úspěšně upravena.',
+	'site_deleted_successfully' => 'Stránka úspěšně odstraněna.',
+	
+	// Errors
+	'site_name_required' => 'Je vyžadován název stránky.',
+	'site_url_required' => 'Je vyžadována URL stránky.',
+	'site_name_minimum' => 'Název stránky musí obsahovat alespoň 2 znaky.',
+	'site_url_minimum' => 'URL stránky musí obsahovat alespoň 10 znaků.',
+	'site_name_maximum' => 'Název stránky může obsahovat maximálně 64 znaků.',
+	'site_url_maximum' => 'URL stránky může obsahovat maximálně 255 znaků.',
+	
+	// Update alerts
+	'new_update_available_x' => 'Je dostupná nová aktualizace doplňku {x}',
+	'new_urgent_update_available_x' => 'Je dostupná nová závažná aktualizace doplňku {x}. Aktualizujte jak nejdříve je to možné!',
+	'current_version_x' => 'Současná verze doplňku: {x}',
+	'new_version_x' => 'Nová verze doplňku: {x}',
+	'view_resource' => 'Zobrazit stránku doplňku',
+);
