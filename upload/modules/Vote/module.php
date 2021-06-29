a33256b92eaf14277dc8909574c8f00bbce3ed1c
Partydragen <admin@partydragen.com>
2021-06-29 18:25:45 +0200

    Release 2.3.0

--- upload/modules/Vote/module.php
+++ upload/modules/Vote/module.php
@@ -3,7 +3,7 @@
  *	Made by Partydragen and Samerton
  *  https://github.com/partydragen/Vote-Module
  *  https://partydragen.com
- *  NamelessMC version 2.0.0-pr9
+ *  NamelessMC version 2.0.0-pr10
  *
  *  License: MIT
  *
@@ -18,8 +18,8 @@ class Vote_Module extends Module {
 		
 		$name = 'Vote';
 		$author = '<a href="https://partydragen.com" target="_blank" rel="nofollow noopener">Partydragen</a>, <a href="https://samerton.me" target="_blank" rel="nofollow noopener">Samerton</a>';
-		$module_version = '2.2.1';
-		$nameless_version = '2.0.0-pr9';
+		$module_version = '2.3.0';
+		$nameless_version = '2.0.0-pr10';
 		
 		parent::__construct($this, $name, $author, $module_version, $nameless_version);
 		
